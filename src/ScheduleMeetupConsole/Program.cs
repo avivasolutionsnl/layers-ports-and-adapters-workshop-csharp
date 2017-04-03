7f682aa782ea60a60b5d982dfae33536168a2449
Jonne Kats <jonnekats@gmail.com>
2017-04-03 21:50:01 +0200

    Add seperate console application for scheduling
    Can be invoked in its own docker container

---
+++ src/ScheduleMeetupConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScheduleMeetupConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
